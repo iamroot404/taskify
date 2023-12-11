b742477f3bfa5ea4747456e19769dc2c397d06b1
iamroot404 <tuvareagan253@.com>
2023-12-12 00:27:09 +0300

    components setup

--- src/components/ShowTask.js
+++ src/components/ShowTask.js
@@ -1,41 +1,32 @@
-
 export const ShowTask = () => {
+    const tasks = [
+    {id: 10001, name: "TASK A", time: "time"},
+    {id: 10002, name: "TASK B", time: "time"},
+    {id: 10003, name: "TASK C", time: "time"}
+]
   return (
     <section className="showTask" >
         <div className="head">
             <div>
                 <span className="title">Todo</span>
-                <span className="count">0</span>
+                <span className="count">3</span>
             </div>
             <button className="clearAll">Clear All</button>
         </div>
         <ul>
-            <li>
+            {tasks.map((task) =>(
+                <li>
                 <p>
-                    <span className="name">Task A</span>
-                    <span className="time">time</span>
+                    <span className="name">{task.name}</span>
+                    <span className="time">{task.time}</span>
                 </p>
                 <i className="bi bi-pen"></i>
                 <i className="bi bi-trash"></i>
             </li>
+            ))}
+            
 
-            <li>
-                <p>
-                    <span className="name">Task A</span>
-                    <span className="time">time</span>
-                </p>
-                <i className="bi bi-pen"></i>
-                <i className="bi bi-trash"></i>
-            </li>
-
-            <li>
-                <p>
-                    <span className="name">Task A</span>
-                    <span className="time">time</span>
-                </p>
-                <i className="bi bi-pen"></i>
-                <i className="bi bi-trash"></i>
-            </li>
+           
         </ul>
     </section>
   )
